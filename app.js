6cccd262a37867cc5bc0539d151d2ddb34c061a9
gonzalogeraldo <geraldo.gonzalo@gmail.com>
2017-01-26 19:18:18 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+(function(window, angular) {'use strict';
+    !function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","detectLanguage","untranslatePage","bootstrap","prefetch","on","off"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);
+    Localize.initialize({key:"jb9Uvr8ct9xgK",rememberLanguage:true});
+    angular
+        .module('AblLocalizeDropdown')
+        .controller('AblLocalizeDropdownController', AblLocalizeDropdownController)
+        .directive('ablLocalizeDropdownInit', ablLocalizeDropdownInit);
+    
+    function AblLocalizeDropdownController($scope){
+        console.log("AblLocalizeDropdownController", $scope);
+    }
+                            
+    function ablLocalizeDropdownInit(){
+        var directive = {
+            restrict: 'E',
+            scope: {},
+            template: '',
+            link: link,
+            controller: controller
+        };
+        function link(scope, element, attrs){
+            console.log("ablLocalizeDropdownInit:link", scope, element, attrs);
+        }
+        function controller($scope){
+            console.log("ablLocalizeDropdownInit:controller", $scope);
+        }
+        return directive;
+    }
+    
+})(window, window.angular);
